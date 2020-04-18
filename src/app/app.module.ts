a7edc4d85299133ffb76c4c0237fe5ba31c4ca18
Eduardo Pardo <eduardo.sanllorente@gmail.com>
2020-04-18 21:54:50 +0200

    feat(board): set board background
    set board background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent, BoardContainerComponent } from 'src/app/components';
+import { RouterModule } from '@angular/router';
+import { routes } from 'src/app/routes';
 
 @NgModule({
   declarations: [
@@ -13,8 +15,10 @@ import { BoardComponent, BoardContainerComponent } from 'src/app/components';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { BoardContainerComponent } from 'src/app/components';
+
+export const routes: Routes = [
+    { path: '', component: BoardContainerComponent}
+]
